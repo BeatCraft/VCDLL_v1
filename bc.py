eb7a511ace492774ed4a05abccac06abba9cef5c
Masashi Takemoto <lesser@beatcraft.jp>
2021-05-24 18:52:31 +0900

    Update bc.py

--- bc.py
+++ bc.py
@@ -18,8 +18,9 @@ import VCDLL
 
 class BCDeviceList:
     device_list = [0];  # 0 - 1
-    ld_list = [0, 1, 2];  # 0 - 2
-    sensor_list = [0, 1, 2, 3]; # 0 - 8
+    ld_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];  # 0 - 15
+    sensor_list = [0, 1, 2, 3, 4, 5, 6, 7]; # 0 - 8
+    ld_by_sensor =[ [0, 1, 2],  [4, 5, 6], [8, 9, 10], [12, 13, 14]]
 
 def BCPreparation(vc):
     pass
@@ -39,38 +40,46 @@ def BCCaptureTest():
     vc.initialize()
 
     print("# start ########################################################");
+    print("# config")
     for dev_id in dl.device_list:
-        print("### device:",dev_id);
         for sensor_id in dl.sensor_list:
             vc.select_sensor(dev_id , sensor_id)
             vc.set_exposure(dev_id, exposure)
             vc.set_gain(dev_id, gain)
         #
+        for ld_id in dl.ld_list:
+            res = vc.select_laser(dev_id, ld_id)
+            res = vc.set_current_laser_setting(dev_id, current, duration)
+        #
+    #
+    print("# capture")
+    for dev_id in dl.device_list:
+        print("### device:", dev_id);
         vc.start_device(dev_id);
         for sensor_id in dl.sensor_list:
             print("### sensor:", sensor_id)
             vc.select_sensor(dev_id , sensor_id)
-            #vc.set_exposure(dev_id, exposure)
-            #vc.set_gain(dev_id, gain)
-            
+            #
             buff_list = []
             for ld_id in dl.ld_list:
-                print("### ld:",ld_id);
-                res = vc.select_laser(dev_id, sensor_id*3+ld_id)
-                res = vc.set_current_laser_setting(dev_id, current, duration)
-                vc.set_laser_onoff(dev_id , sensor_id, 1)
+                print("### ld:", ld_id);
+                res = vc.select_laser(dev_id, +ld_id)
+                vc.set_laser_onoff(dev_id, sensor_id, 1)
+                #
                 buff = vc.get_buffer(dev_id, sensor_id, 10000)
+                #
                 vc.set_laser_onoff(dev_id , sensor_id, 0)
                 print(type(buff));
                 buff_list.append(buff)
             #
             k = 0
-            for ld_id in dl.ld_list:
+            for ld_id in dl.ld_by_sensor[sensor_id/4]:
                 fname = "cap_" + str(dev_id) + "_" + str(sensor_id) +"_" + str(ld_id) + ".raw";
                 print("### fname:",fname);
                 image = ctypes.string_at(buff_list[k] , 5664 * 4248 * 2)
                 with open(fname, 'wb') as f:
                     f.write(image)
+                #
                 #pname = fname + ".png"
                 #pil = Image.frombytes(mode = 'L', size = (5664, 4248), data = image)
                 #pil.save(pname)
